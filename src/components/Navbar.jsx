00779361fdb06f2c8faf1c3354e7831f1a21eae2
javtr <javtriana.z@gmail.com>
2023-12-11 22:11:17 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/img/logo_nav.png";
 
 export default function Navbar() {
   return (
     <div className="navbar init">
       <div className="navbar__content init__c">
+
         <Link className="navbar__content--logo" to="/">
-          Logo
+          <img src={logo} alt="Logo" />
         </Link>
+
         <div className="navbar__content--links">
           <Link to="/education">Education</Link>
           <Link to="/buy">Buy</Link>
