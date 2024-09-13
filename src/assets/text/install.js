1cba47a7c07dda3b30f8381abd7e26c990f0b89c
javtr <javtriana.z@gmail.com>
2024-09-13 12:38:58 -0500

    inst

--- src/assets/text/install.js
+++ src/assets/text/install.js
@@ -1,17 +1,29 @@
 export const textEn = {
+  titg1: "1. Indicator Installation",
   tit1: "Normal Installation",
   tit2: "Installation with Exceptions (Recommended)",
   subt2: "In certain cases, antivirus software may identify the files as false positives. To resolve this issue, it is recommended to follow these steps:",
-  tit3: "Uninstall the Indicators",
+  tit3: "Uninstall Indicators",
   subt3: "",
+  titg2: "2. Common Errors",
+  tit4: "The indicator is not displayed on the chart, but the side button is visible.",
+  subt4: "- Check for any errors while loading the indicator and obtain the code. To do this, follow the steps in the video.",
+  tit5: "The indicator does not show any content, not even the side button.",
+  subt5: "- Check the error while loading the indicator and follow the steps in the video.",
 };
 
 
-export const textEs =
-{
-  tit1:"Instalación normal",
-  tit2:"Instalación con excepciones (recomendada)",
-  subt2:"En ciertos casos, el software antivirus puede identificar los archivos como falsos positivos. Para solucionar este problema, se recomienda seguir estos pasos:",
-  tit3:"Desinstalar los indicadores",
-  subt3:"",
+
+export const textEs = {
+  titg1: "1. Instalación de indicadores",
+  tit1: "Instalación normal",
+  tit2: "Instalación con excepciones (recomendada)",
+  subt2: "En ciertos casos, el software antivirus puede identificar los archivos como falsos positivos. Para solucionar este problema, se recomienda seguir estos pasos:",
+  tit3: "Desinstalar indicadores",
+  subt3: "",
+  titg2: "2. Errores Comunes",
+  tit4: "El indicador no se muestra en el gráfico, pero el botón lateral está visible.",
+  subt4: "- Verificar que no haya errores al cargar el indicador y obtener el código. Para esto, sigue los pasos del video.",
+  tit5: "El indicador no muestra ningún contenido, ni siquiera el botón lateral.",
+  subt5: "- Verificar el error al cargar el indicador y seguir los pasos del video.",
 };
--- src/pages/Install.jsx
+++ src/pages/Install.jsx
@@ -1,56 +1,106 @@
 import React, { useState, useContext, useEffect } from "react";
-import vid_normal_es from "../assets/vids/ins_norm_d.mp4";
-import vid_secure_es from "../assets/vids/ins_seg_d.mp4";
-import vid_desinst_es from "../assets/vids/remove_d.mp4";
-import vid_normal_en from "../assets/vids/ins_norm_d.mp4";
-import vid_secure_en from "../assets/vids/ins_seg_d.mp4";
-import vid_desinst_en from "../assets/vids/remove_d.mp4";
+import vid_normal_es from "../assets/video/normal.mp4";
+import vid_secure_es from "../assets/video/secure.mp4";
+import vid_desinst_es from "../assets/video/uninstall.mp4";
+import vid_noDllCode_es from "../assets/video/no_dll_cod.mp4";
+import vid_noDll_es from "../assets/video/no_dll.mp4";
+
+import vid_normal_en from "../assets/video/normal_en.mp4";
+import vid_secure_en from "../assets/video/secure_en.mp4";
+import vid_desinst_en from "../assets/video/uninstall_en.mp4";
+
+import vid_noDllCode_en from "../assets/video/no_dll_cod_en.mp4";
+import vid_noDll_en from "../assets/video/no_dll_en.mp4";
+
 
 import LanguageContext from "../context/langContext";
 
 import { textEn, textEs } from "../assets/text/install";
 
 export default function Install() {
-  const { lang, setLang } = useContext(LanguageContext);
+  const { lang } = useContext(LanguageContext);
   const [text, setText] = useState([]);
   const [isloading, setIsloading] = useState(true);
+  const [langLoaded, setLangLoaded] = useState("en");
 
   useEffect(() => {
     if (lang === "en") {
       setText(textEn);
+      setLangLoaded("en");
     } else if (lang === "es") {
       setText(textEs);
+      setLangLoaded("es");
     } else {
       setText(textEn);
+      setLangLoaded("en");
     }
 
     setIsloading(false);
   }, [lang]);
 
-
-
   return (
+
+    
+      isloading===true ?
+      <></>
+      :
+
     <div className="installation">
       <div className="installation__container">
+        <h2 className="installation__container--tit1">{text.titg1}</h2>
 
-        <h2 className="installation__container--tit1">{text.tit1}</h2>
+        <h3 className="installation__container--tit1">{text.tit1}</h3>
         <video controls>
-          <source src={lang === "en" ? vid_normal_en : vid_normal_es} type="video/mp4" />
+          <source
+            src={lang === "en" ? vid_normal_en : vid_normal_es}
+            type="video/mp4"
+          />
         </video>
 
-        <h2 className="installation__container--tit1">{text.tit2}</h2>
+        <h3 className="installation__container--tit1">{text.tit2}</h3>
         <p className="installation__container--p">{text.subt2}</p>
         <video controls>
-          <source src={lang === "en" ? vid_secure_en : vid_secure_es} type="video/mp4" />
+          <source
+            src={lang === "en" ? vid_secure_en : vid_secure_es}
+            type="video/mp4"
+          />
         </video>
 
-
-        <h2 className="installation__container--tit1">{text.tit3}</h2>
+        <h3 className="installation__container--tit1">{text.tit3}</h3>
         <video controls>
-          <source src={lang === "en" ? vid_desinst_es : vid_desinst_es} type="video/mp4" />
+          <source
+            src={lang === "en" ? vid_desinst_en : vid_desinst_es}
+            type="video/mp4"
+          />
         </video>
+
+
+        <h2 className="installation__container--tit1">{text.titg2}</h2>
+
+        <h3 className="installation__container--tit1">{text.tit4}</h3>
+        <p className="installation__container--p">{text.subt4}</p>
+        <video controls>
+          <source
+            src={lang === "en" ? vid_noDllCode_en : vid_noDllCode_es}
+            type="video/mp4"
+          />
+        </video>
+
+
+        <h3 className="installation__container--tit1">{text.tit5}</h3>
+        <p className="installation__container--p">{text.subt5}</p>
+        <video controls>
+          <source
+            src={lang === "en" ? vid_noDll_en : vid_noDll_es}
+            type="video/mp4"
+          />
+        </video>
+
 
       </div>
     </div>
+
+
+
   );
 }
