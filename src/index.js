1eaf607fc018c55ed601563b7cf84fa4a24b650a
javtr <javtriana.z@gmail.com>
2025-07-28 22:50:08 -0500

    browser

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  // <BrowserRouter>
+  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+    </Router>
+  // </BrowserRouter>
 );
 
 reportWebVitals();
