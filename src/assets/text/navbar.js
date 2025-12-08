68c962284a96e64fe4910bb4ee49a535c7ef0561
javtr <javtriana.z@gmail.com>
2025-12-07 22:25:57 -0500

    previo idioma

--- src/assets/text/navbar.js
+++ src/assets/text/navbar.js
@@ -2,6 +2,7 @@ export const textEn =
   {
     dec:"Risk",
     ind:"Indicators",
+    free:"Free",
     edu:"Education",
     buy:"Buy",
     con:"Contact",
@@ -12,6 +13,7 @@ export const textEs =
   {
     dec:"Declaraciones",
     ind:"Indicadores",
+    free:"Gratis",
     edu:"Educación",
     buy:"Comprar",
     con:"Contacto",
